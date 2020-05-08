1ef85cdb8b67c4faa9ec22a593022e44a4cda8a6
Damian K <damian-8-8@o2.pl>
2020-05-08 21:41:19 +0200

    Z score outliers implemented

--- src/main/java/com/pracownia/vanet/model/protection/SDBGHAlgorithm.java
+++ src/main/java/com/pracownia/vanet/model/protection/SDBGHAlgorithm.java
@@ -1,5 +1,6 @@
 package com.pracownia.vanet.model.protection;
 
+import java.lang.Math;
 import com.pracownia.vanet.model.devices.Device;
 import com.pracownia.vanet.model.devices.RoadSide;
 
@@ -15,12 +16,44 @@ public class SDBGHAlgorithm {
         this.devices = devices;
     }
 
+    private double mean() {
+        double result = 0.0;
+        int counter = 0;
+
+        for (Device d: this.devices) {
+            if (d.getShouldTransfer().doubleValue() > 0) {
+                result += d.getTransferred().doubleValue() / d.getShouldTransfer().doubleValue();
+                counter++;
+            }
+        }
+
+        return result / counter;
+    }
+
+    private double deviation(double mean) {
+        double result = 0.0;
+        int counter = 0;
+
+        for (Device d: this.devices) {
+            if (d.getShouldTransfer().doubleValue() > 0) {
+                double value = d.getTransferred().doubleValue() / d.getShouldTransfer().doubleValue();
+                result += Math.pow(value - mean, 2);
+                counter++;
+            }
+        }
+
+        return Math.sqrt(result / counter);
+    }
 
     public List<Device> runAnalysis() {
-         return devices.stream()
+        double mean = this.mean();
+        double deviation = this.deviation(mean);
+
+        return devices.stream()
                  .filter(d -> d.getShouldTransfer().get() != 0 &&
-                         !(d instanceof RoadSide) &&
-                         d.getTransferred().doubleValue() / d.getShouldTransfer().doubleValue() < ratio)
+                         !(d instanceof RoadSide) && Math.abs(
+                             ((d.getTransferred().doubleValue() / d.getShouldTransfer().doubleValue()) - mean) / deviation
+                     ) >= 1)
                  .collect(Collectors.toList());
     }
 }
